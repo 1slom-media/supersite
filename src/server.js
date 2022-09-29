5b713cd55921c27fb2fa55a1bdc68314b21c718a
1slom_media <islombektagayev1@gmail.com>
2022-09-29 15:54:12 +0500

    add cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { PORT } from "./config.js";
+import cors from 'cors';
 import modules from "./modules/index.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(modules);
 
 app.listen(PORT, () => console.log(`server ready at http://localhost:${PORT}`));
